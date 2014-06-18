b697f063dc15503b1cc868bcceb9dca70c5b0f07
LegNBass <charmentroutv@gmail.com>
2014-06-18 11:30:35 -0400

    Updated To Meet All Requirements

--- Main.py
+++ Main.py
@@ -1,23 +1,26 @@
-import os
 import sys
 from PML_Parser import parsePML
 
 if __name__ == "__main__":
 
-	#Check for valid argument
+	#Check for valid doc argument
 	if len(sys.argv)>1 and sys.argv[1][-5:] == '.html':
 
-		#Run PML Parser
+		#Retrieve text from file and run the parsing script
 		f = open(sys.argv[1])
 		text = f.read()
 		result = parsePML(text)
 
-		for i in xrange(len(result.split('\n'))-1):
-			text = text[:text.find('<pml>')] + result.split('\n')[i] + text[text.find('</pml>')+6:]
+		# Each line of the result represents a PML block, so we iterate 
+		# through each line and inject it into each PML block in order
+		for i in xrange(len(result.split('\n'))-1): # -1 here because exec adds a blank line
+			text = text[:text.find('<pml>')] + result.split('\n')[i] + text[text.find('</pml>')+6:] # +6 here to avoid displaying pml tag
 		print text
 
+	# Display syntax when asked for help
 	elif len(sys.argv)>1 and sys.argv[1] == '--help':
-		print 'Syntax is "python Main.py [HTML Document FilePath]"'
+		print 'Syntax is: "python Main.py [HTML Document FilePath]"'
 
+	# illegal arg result.
 	else: 
 		print 'No Valid Arguments\n--help for more info'
--- PML_Parser.py
+++ PML_Parser.py
@@ -1,9 +1,27 @@
 import sys
+import re
 from StringIO import StringIO
-#
+
+
+# This Method finds the indentation used and returns the 
+# indent of the first letter in the first non-whitespace line
+def findIndent(text): 
+	pBlankLine = re.compile('\s*$') #Blank Line Regex
+	pText = re.compile('\w') #Any non-whitespace character Regex
+	
+	# Split the input into rows and iterate
+	rows = text.split('\n')	
+	for i in rows:
+		if not pBlankLine.match(i): break # Break when first non-whitespace line is found
+	if pText.search(i): #Error trapping
+		start = pText.search(i).start()
+		return i[:start]
+	else:
+		return ''
+
+
 # This Method takes a file input and parses it for 
 # <pml> python code </pml>
-#
 def parsePML(htmlFile):
 	# pml will hold the entire statement to execute
 	pml=''
@@ -12,29 +30,40 @@ def parsePML(htmlFile):
 	# This is to help parse for the pml tags, regarless of case.
 	ltext = htmlFile.lower()
 
-	# indicies will contain a set of tuples of the start and end of each pml block
-	# start and end will temporarily hold the values of each tuple
-	indicies=[]
+	# Start and end will temporarily hold the indicies of each PML block
 	start, end = ltext.find('<pml>'), ltext.find('</pml>')
 
-	# 
+	# Loop while there are still valid PML blocks
 	while start != -1 and end != -1:
-		indicies.append((start,end))
+
+		#Save the interior of the PML block as statement
 		statement = htmlFile[start+6:end] 
+
+		#find next indicies
 		start = ltext.find('<pml>',end)
-		end = ltext.find('</pml>', end+6)
-		while statement[:4]=='    ':
-			statement = '\n'.join([i[4:] for i in statement.split('\n')])+'\nprint pml.replace("\\n","")\n' if 'pml' in statement \
-				else '\n'.join([i[1:] for i in statement.split('\n')])
-		while statement[:1]=='\t':
-			statement = '\n'.join([i[1:] for i in statement.split('\n')])+'\nprint pml.replace("\\n","")\n' if 'pml' in statement \
-				else '\n'.join([i[1:] for i in statement.split('\n')])
+		end = ltext.find('</pml>', end+6) # start the search after the existing tags close
+
+		# Finds the indent of the PML block
+		indent = findIndent(statement)
+
+		# Removes indentation and adds a print pml statement if the pml variable is reassigned
+		pAssignPML = re.compile('pml\s*[+\-*%/]?=') #finds pml assignment
+		while statement[:len(indent)] == indent and indent != '':
+			statement = '\n'.join([i[len(indent):] for i in statement.split('\n')])+'\nprint pml.replace("\\n","")\n' if pAssignPML.search(statement) \
+				else '\n'.join([i[len(indent):] for i in statement.split('\n')])
+
+		# Adds statement to pml expression
 		pml+=statement
+	
+	# Compiles the pml expression into a code object
 	result = compile(pml,'-','exec')
 
+	# Creates a buffer and replaces the stdout with it during 
+	# the execution of the pml expression
 	buffer = StringIO()
 	sys.stdout = buffer
 	exec result
-	sys.stdout = sys.__stdout__
+	sys.stdout = sys.__stdout__ #replaces the stdout
 
+	# Returns the output of the interpreted pml expression
 	return (buffer.getvalue())
